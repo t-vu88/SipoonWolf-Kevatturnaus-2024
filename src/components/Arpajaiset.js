d7cf740fbee0bc3fa4f7dded11935945870cbe12
Thu Trang Vu <vuthutrang2811@gmail.com>
2024-05-05 16:57:29 +0300

    fixed typo

--- src/components/Arpajaiset.js
+++ src/components/Arpajaiset.js
@@ -9,14 +9,14 @@ const Arpajaiset = () => {
     <div style ={paragraphStyle}>
       <p>Tule mukaan jännitykseen ja voita mahtavia palkintoja Turnauksen arpajaisissa! </p>
 
-    <p>Arpajaiset järjestetään kahvion vieressä.Yksi arpa maksaa 2€, ja kolme arpaa on sinun vain 5€:lla.</p>
+    <p>Arpajaiset järjestetään kahvion vieressä. Yksi arpa maksaa 2€, ja kolme arpaa on sinun vain 5€:lla.</p>
      <p>  Palkintoina mm. </p>
      <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
      <li>Käärijän allekirjoitamat kiekot (30kpl)</li>
      <li>Pekka Rinteen allekirjoitamat kiekot (2kpl)</li>
       <li>Maila</li>
       <li>Ravintolalahjakortteja</li>
-      <li>Hierontalahjakortteka</li>
+      <li>Hierontalahjakortteja</li>
       <li>50€ nesteen polttoainelahjakortti</li>
       <li>----</li>
       <li>----</li>
@@ -25,7 +25,7 @@ const Arpajaiset = () => {
       
     
 
-    <p>Älä jätä tilaisuuttasi käyttämättä – tule, osta arpa ja liity voittajien joukkoon!</p>
+    <p>Älä jätä tilaisuuttasi käyttämättä – osta arpa ja liity voittajien joukkoon!</p>
     </div>
     
   );

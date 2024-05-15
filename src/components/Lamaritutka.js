29f92c83b7ea7bac6617d375d40c6535086c6be0
Thu Trang Vu <vuthutrang2811@gmail.com>
2024-05-15 10:39:38 +0300

    adding tables

--- src/components/Lamaritutka.js
+++ src/components/Lamaritutka.js
@@ -1,24 +1,80 @@
-
 import React from 'react';
 
 const Lamaritutka = () => {
+  const data = {
+    u8: [
+      { name: 'Pelaja ', speed: 0 },
+      { name: 'Pelaja ', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 }
+    ],
+    u7: [
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 }
+    ],
+    others: [
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 },
+      { name: 'Pelaaja', speed: 0 }
+    ]
+  };
+
+  const sortPlayersBySpeed = (players) => {
+    return players.sort((a, b) => b.speed - a.speed);
+  };
+
+  const renderTable = (category, players) => (
+    <div key={category}>
+      <h3>{category.toUpperCase()}</h3>
+      <table className='ranking-table'>
+        <thead>
+          <tr>
+            <th>Sija</th>
+            <th style={{ width: '60%' }}>Pelaaja</th>
+            <th style={{ width: '20%' }}>Laukauksen nopeus</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortPlayersBySpeed(players).map((player, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{player.name}</td>
+              <td>{player.speed}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+
   const paragraphStyle = {
     color: '#F0AE2E',
     textAlign: 'center'
   };
   const paragraphStyle1 = {
-    fontSize:'18px'
+    fontSize: '18px'
   };
-  
-  return (
-    <div>
-      
 
+  return (
+    <div className="lämäritutka">
       <h3 style={paragraphStyle}>Lämäritutka</h3>
       <div style={paragraphStyle1}>
-      <p>Tule testaamaan laukauksesi nopeutta ja voimaa. 
-      Lämäritutkassa on molempana turnauspäivänä vetokilpailu, jossa 2€:lla pääsee vetämään kolmesti. 
-        Päivän kovin laukaus saa muhkean karkkiämpärin. </p>    
+        <p>
+          Tule testaamaan laukauksesi nopeutta ja voimaa. Lämäritutkassa on molempana turnauspäivänä vetokilpailu, jossa 2€:lla pääsee vetämään kolmesti.
+          Päivän kovin laukaus saa muhkean karkkiämpärin.
+        </p>
+      </div>
+      <div>
+        {renderTable('U7', data.u7)}
+        {renderTable('U8', data.u8)}
+        
+        {renderTable('Muut', data.others)}
       </div>
     </div>
   );

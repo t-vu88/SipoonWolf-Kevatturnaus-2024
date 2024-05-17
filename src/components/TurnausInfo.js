65886db51e1670319352eed11b066ce95412f12a
Thu Trang Vu <vuthutrang2811@gmail.com>
2024-05-18 00:43:26 +0300

    adding text

--- src/components/TurnausInfo.js
+++ src/components/TurnausInfo.js
@@ -23,14 +23,16 @@ Hallin läheisyydessä on runsaasti pysäköintipaikkoja, joten saapuminen paika
 <p>Turnauspäivänä on paljon muutakin kuin otteluita. Varaamme aikaa myös yhteiselle hauskanpidolle ja virkistykselle. 
   Kahviosta saa sekä suolaista että makeaa koko viikonlopun ajan, lisäksi on arpajaisia, onnenpyörää, lämäritutkaa sekä herkullista grillattua makkaraa.
   Hallilla on myös teroitusmahdollisuus hintaan 5 €/pari (tiedustelut kahviosta). </p>
-
-<p>Haluamme kiittää lämpimästi tapahtuman sponsoreita, jotka ovat mahdollistaneet tämän upean turnauksen:</p>
+  <p> Muista jakaa kuvia, tarinoita  ja parhaat hetkesi turauksestamme instagramissa hastageilla<span style={{color:'#F0AE2E'}} > #sipoonwolf_2016</span> tai <span style={{color:'#F0AE2E'}} >#u7u8kevätturnaus2024</span>  ja voita arvokas Tapwell-hana. </p>
+<p> Lopuksi haluamme kiittää lämpimästi tapahtuman sponsoreita, jotka ovat mahdollistaneet tämän upean turnauksen:</p>
 <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
     <li>Corrector Oy</li>
     <li>Prodessendum Oy</li>
     </ul>
 
 <p>Lisätietoja turnauksesta ja sen ohjelmasta löydätte yllä olevista välilehdistä.</p>
+
+
 </div>
       </di>
     </div>

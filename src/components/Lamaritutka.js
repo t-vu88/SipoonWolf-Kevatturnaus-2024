5a2b9cc26e6977c56fc9bfe3565b548cd929a97a
Thu Trang Vu <vuthutrang2811@gmail.com>
2024-05-18 22:39:16 +0300

    edit text

--- src/components/Lamaritutka.js
+++ src/components/Lamaritutka.js
@@ -2,50 +2,6 @@ import React from 'react';
 
 const Lamaritutka = () => {
       /* you can add the result here, type carefully  */
-  const data = {
-    u8: [
-      { name: 'Mikael Beckström', speed: 88 },
-      { name: 'Pelaaja ', speed: 0 },
-      { name: 'Pelaaja', speed: 0 },
-      { name: 'Pelaaja', speed: 0 },
-      { name: 'Pelaaja', speed: 0 }
-    ],
-    others: [
-      { name: 'Pelaaja', speed: 0 },
-      { name: 'Pelaaja', speed: 0 },
-      { name: 'Pelaaja', speed: 0 },
-      { name: 'Pelaaja', speed: 0 },
-      { name: 'Pelaaja', speed: 0 }
-    ]
-  };
-
-  const sortPlayersBySpeed = (players) => {
-    return players.sort((a, b) => b.speed - a.speed);
-  };
-
-  const renderTable = (category, players) => (
-    <div key={category}>
-      <h3>{category.toUpperCase()}</h3>
-      <table className='ranking-table'>
-        <thead>
-          <tr>
-            <th>Sija</th>
-            <th style={{ width: '60%' }}>Pelaaja</th>
-            <th style={{ width: '20%' }}>Laukauksen nopeus</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortPlayersBySpeed(players).map((player, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{player.name}</td>
-              <td>{player.speed}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
 
   const paragraphStyle = {
     color: 'white',
@@ -64,12 +20,6 @@ const Lamaritutka = () => {
         <p>Lämäritutkassa on molempana turnauspäivänä vetokilpailu, jossa 2€:lla pääsee vetämään kolmesti.
            U7/U8-ryhmän voittajalle palkinnoksi ämpärillinen herkkuja ja Muut-ryhmän voittajalle palkinnoksi allekirjoitettu kiekko.
         </p>
-      </div>
-      <div>
-       
-        {renderTable('U7/U8', data.u8)}
-        
-        {renderTable('Muut', data.others)}
       </div>
     </div>
   );

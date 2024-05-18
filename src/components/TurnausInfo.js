17a23704288437f787b143a6cb6cc2e2468d6b1d
Thu Trang Vu <vuthutrang2811@gmail.com>
2024-05-18 08:37:44 +0300

    adding text

--- src/components/TurnausInfo.js
+++ src/components/TurnausInfo.js
@@ -17,13 +17,15 @@ const TurnausInfo = () => {
                <p>Pelit pelataan Sipoon jäähallissa (Urheilutie 2, 04130 Sipoo). 
 Hallin läheisyydessä on runsaasti pysäköintipaikkoja, joten saapuminen paikalle autolla on vaivatonta.</p>
 
-<p>Katsomossa on runsaasti istumapaikkoja, 
-  joten rohkaisemme teitä tuomaan mukananne koko perheen nauttimaan jääkiekon taikasta ja kannustamaan nuoria urheilijoitamme. 
-</p>
 <p>Turnauspäivänä on paljon muutakin kuin otteluita. Varaamme aikaa myös yhteiselle hauskanpidolle ja virkistykselle. 
   Kahviosta saa sekä suolaista että makeaa koko viikonlopun ajan, lisäksi on arpajaisia, onnenpyörää, lämäritutkaa sekä herkullista grillattua makkaraa.
   Hallilla on myös teroitusmahdollisuus hintaan 5 €/pari (tiedustelut kahviosta). </p>
-  <p> Muista jakaa kuvia, tarinoita  ja parhaat hetkesi turauksestamme instagramissa hastageilla<span style={{color:'#F0AE2E'}} > #sipoonwolf_2016</span> tai <span style={{color:'#F0AE2E'}} >#u7u8kevätturnaus2024</span>  ja voita arvokas Tapwell-hana. </p>
+
+<p>Katsomossa on runsaasti istumapaikkoja, 
+  joten rohkaisemme teitä tuomaan mukananne koko perheen nauttimaan jääkiekon taikasta ja kannustamaan nuoria urheilijoitamme. 
+  Kerro sitten kuinka onnistuimme ! Jaa kuvia, tarinoita  ja parhaat hetkesi turauksestamme instagramissa 
+    hastageilla<span style={{color:'#F0AE2E'}} > #sipoonwolf_2016</span> tai <span style={{color:'#F0AE2E'}} >#u7u8kevätturnaus2024</span>  ja voita arvokas Tapwell-hana.
+    </p>
 <p> Lopuksi haluamme kiittää lämpimästi tapahtuman sponsoreita, jotka ovat mahdollistaneet tämän upean turnauksen:</p>
 <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
     <li>Corrector Oy</li>
